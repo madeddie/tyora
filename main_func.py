f8d96cc78ffff68dfd0caf891ef429d639a991da
Edwin Hermans <edwin@madtech.cx>
2024-05-02 11:45:37 -0400

    Adding argparse, some refactoring

--- main_func.py
+++ main_func.py
@@ -6,7 +6,7 @@
 # TODO: UI to config username and password
 # TODO: UI for checking exercise description
 # TODO: UI for submitting solutions
-from html.parser import HTMLParser
+import argparse
 from getpass import getpass
 import json
 import urllib.parse
@@ -17,12 +17,21 @@ from pathlib import Path
 
 import htmlement
 
+
+def parse_args(args: list | None = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Interact with mooc.fi CSES instance')
+    parser.add_argument('--username', help='tmc.mooc.fi username')
+    parser.add_argument('--password', help='tmc.mooc.fi password')
+
+    return parser.parse_args(args)
+
 def read_config(config_file: Path) -> dict:
     with open(config_file, "r") as f:
         config = json.load(f)
         for setting in ("username", "password"):
             assert setting in config
     return config
+
 
 # TODO: not quite functional, move input/getpass to main when implmenting argparse
 def write_config(config_file: Path, username: str, password: str) -> None:
@@ -34,23 +43,32 @@ def write_config(config_file: Path, username: str, password: str) -> None:
     }
     print("Writing config to file")
     with open(config_file, "w") as f:
-       json.dump(config, f)
+        json.dump(config, f)
 
-def create_session(cookiejar: http.cookiejar.CookieJar) -> urllib.request.OpenerDirector:
+
+def create_session(
+    cookiejar: http.cookiejar.CookieJar,
+) -> urllib.request.OpenerDirector:
     return urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookiejar))
 
-def open_url(url: str, session: urllib.request.OpenerDirector, referer: str = "", data: bytes | None = None) -> http.client.HTTPResponse:
+
+def open_url(
+    url: str,
+    session: urllib.request.OpenerDirector,
+    referer: str = "",
+    data: bytes | None = None,
+) -> http.client.HTTPResponse:
     if referer:
         session.addheaders = [("referer", referer)]
     res = session.open(url, data)
 
     return res
 
+
 # NOTE: maybe split this up in multiple functions, this way they become easier to test by simply inject HTML
 # - find_login_url (also useable as input for logged_in() -> bool checker
 # - find_login_form or parse_form in login page
 # - submit_login_form
-
 def find_link(html: str, xpath: str) -> str | None:
     '''Search for html link by xpath and return the href attribute'''
     anchor_element = htmlement.fromstring(html).find(xpath)
@@ -73,11 +91,13 @@ def submit_form(session: urllib.request.OpenerDirector, url: str, data: dict) ->
     form_data = urllib.parse.urlencode(data).encode("ascii")
     session.open(url, data=form_data)
 
-def login(session: urllib.request.OpenerDirector, username: str, password: str, base_url: str) -> urllib.request.OpenerDirector | None:
+def login(
+    session: urllib.request.OpenerDirector, username: str, password: str, base_url: str
+) -> urllib.request.OpenerDirector | None:
     res = session.open(base_url)
     login_element = htmlement.parse(res).find('.//a[@class="account"]')
     if login_element is not None:
-        login_url = urllib.parse.urljoin(res.url, login_element.get('href'))
+        login_url = urllib.parse.urljoin(res.url, login_element.get("href"))
     else:
         raise ValueError("Failed to find login_url")
 
@@ -88,8 +108,8 @@ def login(session: urllib.request.OpenerDirector, username: str, password: str, 
     if form_element is not None:
         action = form_element.get('action')
         form_data = dict()
-        for form_input in form_element.iter('input'):
-            form_data[form_input.get('name')] = form_input.get('value', '')
+        for form_input in form_element.iter("input"):
+            form_data[form_input.get("name")] = form_input.get("value", "")
     else:
         raise ValueError("Failed to find form")
 
@@ -99,17 +119,20 @@ def login(session: urllib.request.OpenerDirector, username: str, password: str, 
 
     session.addheaders = [("referer", res.url)]
     res = session.open(urllib.parse.urljoin(res.url, action), data=form_data)
-    
+
     return session
 
+
 def main():
+    args = parse_args()
+    print(args)
     # TODO: make base_url configurable based on course
     base_url = "https://cses.fi/dsa24k/list/"
-    config_file = Path.home() / '.config' / 'moocfi_cses' / 'config.json'
-    state_dir = Path.home() / '.local' / 'state' / 'moocfi_cses'
+    config_file = Path.home() / ".config" / "moocfi_cses" / "config.json"
+    state_dir = Path.home() / ".local" / "state" / "moocfi_cses"
 
     config = read_config(config_file)
-    cookiejar = http.cookiejar.LWPCookieJar(state_dir / 'cookies.txt')
+    cookiejar = http.cookiejar.LWPCookieJar(state_dir / "cookies.txt")
     try:
         cookiejar.load(ignore_discard=True)
     except FileNotFoundError:
@@ -123,15 +146,16 @@ def main():
     if login_element is not None:
         login_text = login_element.text
     else:
-        login_text = ''
+        login_text = ""
     if not config["username"] in login_text:
-        print('trying to log in')
-        login(session, config['username'], config['password'], base_url)
+        print("trying to log in")
+        login(session, config["username"], config["password"], base_url)
     cookiejar.save(ignore_discard=True)
 
     res = open_url(base_url, session)
     # print(res.read())
     # print(type(res))
+
 
 if __name__ == "__main__":
     main()
